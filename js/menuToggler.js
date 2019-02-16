27203d6e4fd2cee30dccd53c15b68d6ba3cc48bc
bruinen <max.machnik@gmail.com>
2019-02-16 23:32:42 +0100

    Mobile bugs fixed, menu toggler animated

--- js/menuToggler.js
+++ js/menuToggler.js
@@ -1,10 +1,10 @@
 const toggleMenu = () => {
-    const menuToggler = document.querySelector('.navCont__toggler');
-    menuToggler.classList.toggle('navCont__toggler--clicked');
+    const menuToggler = document.querySelector('.hamburger');
+    menuToggler.classList.toggle('is-active');
 
     const nav = document.querySelector('.nav');
     nav.classList.toggle('nav--hidden');
 
-    // const cover = document.querySelector('.cover');
-    // cover.classList.toggle('cover--hidden');
+    const cover = document.querySelector('.cover');
+    cover.classList.toggle('cover--hidden');
 }
